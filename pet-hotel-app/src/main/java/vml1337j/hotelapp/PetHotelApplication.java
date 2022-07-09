e34576ab8b476196a9c12677f9a8be9cceb8ac2c
vml1337j <vml133j@gmail.com>
2022-07-09 16:37:50 +0300

    init pet hotel app

---
+++ pet-hotel-app/src/main/java/vml1337j/hotelapp/PetHotelApplication.java
@@ -0,0 +1,13 @@
+package vml1337j.hotelapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetHotelApplication.class, args);
+	}
+
+}
